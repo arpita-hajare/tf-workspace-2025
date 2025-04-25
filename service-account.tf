4be17c1f80d78b8eaf889b8223a57fe69bc5f2bb
arpita-hajare <95733100+arpita-hajare@users.noreply.github.com>
2025-04-25 12:22:27 +0530

    Update service-account.tf

--- service-account.tf
+++ service-account.tf
@@ -4,3 +4,6 @@ resource "google_service_account" "devops_sa" {
   account_id   = var.service_account_id
   display_name = var.service_account_display_name
 }
+output "service_account_email" {
+  value = google_service_account.devops_sa.email
+}
