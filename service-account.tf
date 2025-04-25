520935d4ad7db4fe42710d621e6de427478cfdd0
arpita-hajare <95733100+arpita-hajare@users.noreply.github.com>
2025-04-25 13:30:30 +0530

    Update service-account.tf

--- service-account.tf
+++ service-account.tf
@@ -4,6 +4,12 @@ resource "google_service_account" "devops_sa" {
   account_id   = var.service_account_id
   display_name = var.service_account_display_name
 }
+
+resource "google_project_iam_member" "devops_sa_roles" {
+  project = var.project_id
+  role    = "roles/editor"  # or more granular like roles/storage.admin, etc.
+  member  = "serviceAccount:${google_service_account.devops_sa.email}"
+}
 output "service_account_email" {
   value = google_service_account.devops_sa.email
 }
